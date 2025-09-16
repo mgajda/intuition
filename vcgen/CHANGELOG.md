eb27f5d979de0e4fbb0aa512431b3bb9d9d52bb6
Aleksy Schubert <alx@mimuw.edu.pl>
2025-09-16 17:59:40 +0200

    moving from Makefile builds do cabal builds

---
+++ vcgen/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for vcgen
+
+## 0.1.0.0 -- 2025-09-16
+
+* First version. Released on an unsuspecting world.
---
+++ vcgen/Setup.hs
@@ -0,0 +1,22 @@
+import Distribution.Simple
+import System.Directory
+import System.Process
+import Distribution.Types.HookedBuildInfo (HookedBuildInfo)
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+  { preConf = \_ _ -> do
+      createDirectoryIfMissing True "app/Tiny"
+
+      -- Run bnfc
+      callProcess "bnfc" ["-m", "--haskell", "-o", "app/Tiny", "Tiny.cf"]
+      
+      -- Run Alex
+      callProcess "alex" ["app/Tiny/LexTiny.x", "--ghc", "-o", "app/Tiny/Lexer.hs"]
+
+      -- Run Happy
+      callProcess "happy" ["app/Tiny/ParTiny.y", "--array",  
+                        "--info", "--ghc", "--coerce", 
+                        "-o", "app/Tiny/ParTiny.hs"]
+      return (Nothing, [])
+  }
